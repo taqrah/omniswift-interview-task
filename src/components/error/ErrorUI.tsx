2060caacb4e3d5ba12de0006d596b0556acfc3a0
Dom-iha <taqib.ibrahim@gmail.com>
2024-02-09 11:25:11 +0100

    Updated errorui styles

--- src/components/error/ErrorUI.tsx
+++ src/components/error/ErrorUI.tsx
@@ -3,7 +3,7 @@ import classes from './error.module.css'
 function ErrorUI({ getStudents }: { getStudents: () => void }) {
   return (
     <div className={classes.error}>
-      <h1 className={classes.error__heading}>Something went wrong</h1>
+      <h2 className={classes.error__heading}>Something went wrong</h2>
       <p className={classes.error__text}>
         Sorry, we could not load the data. Please Check your connection then
         click the button below to try again.
